fae782991c9e74e92ae52112a09aebad1801401c
abdel-17 <abdelrahmanb015@gmail.com>
2023-11-15 20:42:23 +0200

    Add `AddTodo` activity and layout for `TodosActivity`

--- app/src/main/java/com/example/to_dolist/ui/AddTodoActivity.java
+++ app/src/main/java/com/example/to_dolist/ui/AddTodoActivity.java
@@ -1,14 +1,16 @@
-package com.example.to_dolist;
+package com.example.to_dolist.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TodosActivity extends AppCompatActivity {
+import com.example.to_dolist.R;
+
+public class AddTodoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todos);
+        setContentView(R.layout.activity_add_todo);
     }
 }
--- app/src/main/java/com/example/to_dolist/ui/TodosActivity.java
+++ app/src/main/java/com/example/to_dolist/ui/TodosActivity.java
@@ -1,14 +1,16 @@
-package com.example.to_dolist;
+package com.example.to_dolist.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.to_dolist.R;
+
+public class TodosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_todos);
     }
 }
