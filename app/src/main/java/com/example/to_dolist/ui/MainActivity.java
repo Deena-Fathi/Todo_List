96c5a71d6f9c40d7727ab0a1f709c840334866d0
abdel-17 <abdelrahmanb015@gmail.com>
2023-11-16 21:35:14 +0200

    Update splash icon

--- app/src/main/java/com/example/to_dolist/ui/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/ui/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.to_dolist.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.splashscreen.SplashScreen;
 import android.os.Bundle;
-
 import com.example.to_dolist.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
