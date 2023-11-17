39a8209f81ca7f39c77f08bc95bd0a2a8cca44f2
abdel-17 <abdelrahmanb015@gmail.com>
2023-11-17 19:54:32 +0200

    Make `MainActivity` only be shown on first launch

--- app/src/main/java/com/example/to_dolist/ui/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/ui/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.to_dolist.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.to_dolist.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FIRST_LAUNCH_KEY = "first_launch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +22,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final SharedPreferences activityPreferences = getPreferences(Context.MODE_PRIVATE);
+        final boolean firstLaunch = activityPreferences.getBoolean(FIRST_LAUNCH_KEY, true);
+        if (!firstLaunch) {
+            replaceWithTodosActivity();
+            return;
+        }
+
         final Button getStartedButton = findViewById(R.id.button_get_started);
-        getStartedButton.setOnClickListener(v -> replaceWithTodosActivity());
+        getStartedButton.setOnClickListener(v -> {
+            activityPreferences.edit().putBoolean(FIRST_LAUNCH_KEY, false).apply();
+            replaceWithTodosActivity();
+        });
     }
 
     private void replaceWithTodosActivity() {
